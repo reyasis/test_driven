8556894272403e94d8d5418fd9b95d3c867f2a2c
Reynard Asis <reynard.asis@gmail.com>
2016-12-19 22:52:20 +0800

    Added templates Added more test

--- functional_test.py
+++ functional_test.py
@@ -24,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
-        self.assertIn('To-do', self.browser.title)
+        self.assertIn('To-Do list', self.browser.title)
         self.fail('Finish the test')
 
 
